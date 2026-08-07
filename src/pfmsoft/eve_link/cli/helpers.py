4af1b4aa05609143953352ab4e0bad555f36ea2f
Chad Lowe <pfmsoft.dev@gmail.com>
2026-08-06 19:24:54 -0700

    refactor: comment out unused settings helper functions
    * Comment out construct_eve_auth_manager_settings function
    * Comment out construct_api_request_settings function
    * Remove unused import of construct_api_request_settings from main_typer.py
    * Settings are now accessed directly from dependencies instead

--- src/pfmsoft/eve_link/cli/helpers.py
+++ src/pfmsoft/eve_link/cli/helpers.py
@@ -44,30 +44,30 @@ def get_eve_link_settings_from_context(ctx: typer.Context) -> EsiLinkSettings:
     return settings
 
 
-def construct_eve_auth_manager_settings(
-    settings: EsiLinkSettings,
-) -> EveAuthManagerSettings:
-    """Return EveAuthManagerSettings from Eve ESI Link settings.
+# def construct_eve_auth_manager_settings(
+#     settings: EsiLinkSettings,
+# ) -> EveAuthManagerSettings:
+#     """Return EveAuthManagerSettings from Eve ESI Link settings.
 
-    Args:
-        settings: The Eve ESI Link settings.
+#     Args:
+#         settings: The Eve ESI Link settings.
 
-    Returns:
-        EveAuthManagerSettings configured for this application instance.
-    """
-    return settings.eve_auth_manager_settings
+#     Returns:
+#         EveAuthManagerSettings configured for this application instance.
+#     """
+#     return settings.eve_auth_manager_settings
 
 
-def construct_api_request_settings(settings: EsiLinkSettings) -> ApiRequestSettings:
-    """Return ApiRequestSettings from Eve ESI Link settings.
+# def construct_api_request_settings(settings: EsiLinkSettings) -> ApiRequestSettings:
+#     """Return ApiRequestSettings from Eve ESI Link settings.
 
-    Args:
-        settings: The Eve ESI Link settings.
+#     Args:
+#         settings: The Eve ESI Link settings.
 
-    Returns:
-        ApiRequestSettings configured for this application instance.
-    """
-    return settings.api_request_settings
+#     Returns:
+#         ApiRequestSettings configured for this application instance.
+#     """
+#     return settings.api_request_settings
 
 
 @deprecated(
