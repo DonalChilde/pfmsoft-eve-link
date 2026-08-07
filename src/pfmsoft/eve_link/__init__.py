d11cbd452fee03e32ac2ae57635c376f7793508b
Chad Lowe <pfmsoft.dev@gmail.com>
2026-08-06 19:19:28 -0700

    refactor: remove commented debug code from init

--- src/pfmsoft/eve_link/__init__.py
+++ src/pfmsoft/eve_link/__init__.py
@@ -18,7 +18,6 @@ __url__ = "https://github.com/DonalChilde/pfmsoft-eve-link"
 __license__ = "MIT"
 
 
-# foo = ""
 from pfmsoft.eve_link import request_factory
 from pfmsoft.eve_link.esi_link import EsiLink, SimpleRequests
 from pfmsoft.eve_link.esi_request.models import (
